20fff360203f434713c72d904d9ece7d46f98fc5
parvajio <mdparvajmosharof17@gmail.com>
2025-03-29 01:42:42 +0600

    sum obj properties

--- objectPs.js
+++ objectPs.js
@@ -12,15 +12,15 @@ const user = {};
 user.name = "John";
 user["surName"] = "Smith";
 
-user.name = "pete"
+user.name = "pete";
 
 delete user.name;
 
-//checking 
+//checking
 
-for(let key in user){
-    console.log(key)
-    console.log(user[key])
+for (let key in user) {
+  console.log(key);
+  console.log(user[key]);
 }
 
 //222222
@@ -53,14 +53,41 @@ for(let key in user){
 
 // now :
 
-const isEmptyObject = (obj) =>{
-    for (let key in obj) {
-        console.log(false)
-       return false       
-    }
-    console.log(true)
-    return true;
+const isEmptyObject = (obj) => {
+  for (let key in obj) {
+    console.log(false);
+    return false;
+  }
+  console.log(true);
+  return true;
+};
+
+const schedule = {};
+isEmptyObject(schedule);
+
+//333333
+// Sum object properties
+// importance: 5
+// We have an object storing salaries of our team:
+
+// let salaries = {
+//   John: 100,
+//   Ann: 160,
+//   Pete: 130
+// }
+// Write the code to sum all salaries and store in the variable sum. Should be 390 in the example above.
+
+// If salaries is empty, then the result must be 0.
+
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130,
+};
+
+let sum = 0;
+for(let key in salaries){
+    sum += salaries[key];
 }
 
-const schedule = {}
-isEmptyObject(schedule)
+console.log(sum);
